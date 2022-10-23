7ffdc7c6184846e28b7158c8438646a64ea5e76b
Guitomesz <42675646+Guitomesz@users.noreply.github.com>
2022-10-23 12:33:25 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      <Text style={styles.title}>Meu primeiro App     </Text>
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>React Native</Text>
+        <Text style={styles.cardSubtitle}>Criando aplicações para Android e iOS</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2a2a2a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff'
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 12,
+    borderRadius: 3
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  cardSubtitle: {
+    
+  }
 });
